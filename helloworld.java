b5d2faacba3f8ba0cb84f607cf172cf2ee5fd15e
Rohit Thaware <91693439+rohitthaware@users.noreply.github.com>
2022-01-30 17:44:03 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
 public class helloworld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
     }
+
 }
